7af1600c9efe0fe47b42eb8f54bb57912f295a8a
Augusto Couto <jancouto@gmail.com>
2023-04-25 18:03:43 -0300

    Commit inicial com arquivo do projeto me java

---
+++ src/main/java/br/com/cnaj/ProvisionamentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cnaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvisionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvisionamentoApplication.class, args);
+	}
+
+}
